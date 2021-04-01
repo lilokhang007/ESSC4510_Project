e0cd7a0eed4877b896b38d732048606ab804db42
lilokhang <lilokhang007@gmail.com>
2021-04-01 18:28:43 +0800

    Downloading raw data

---
+++ raw.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import requests
+import json
+
+def get_raw_data_from_fetch_request(yyyy='2021'):
+    # Send a request to get a xml file from HKO backend server
+    page = requests.get("https://www.hko.gov.hk/cis/dailyExtract/dailyExtract_{}.xml".format(yyyy), {
+      "headers": {
+        "accept": "text/plain, */*; q=0.01",
+        "accept-language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+        "cache-control": "no-cache",
+        "pragma": "no-cache",
+        "sec-ch-ua": "\"Google Chrome\";v=\"89\", \"Chromium\";v=\"89\", \";Not\\\"A\\\\Brand\";v=\"99\"",
+        "sec-ch-ua-mobile": "?1",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-requested-with": "XMLHttpRequest"
+      },
+      "referrerPolicy": "no-referrer-when-downgrade",
+      "body": None,
+      "method": "GET",
+      "mode": "cors",
+      "credentials": "include"
+    })
+
+    # put the data in form of json
+    return json.loads(page.text)['stn']['data']
+
+if __name__ == "__main__":
+    for year in range(1990, 2022):
+        jsonData = get_raw_data_from_fetch_request(
+            yyyy = str(year)
+        )
+
+        # use year as key
+        df = pd.DataFrame()
+        for monthData in jsonData:
+            df = pd.concat([df, pd.DataFrame(monthData).iloc[:-2]])
+
+        df[['day', 'slp', 'maxtemp', 'avgtemp', 'mintemp', 'dewtemp', 'rh', 'cld', 'rf', 'sunhr', 'prewd', 'avgws']] = pd.DataFrame(df.dayData.tolist())
+        df = df.drop('dayData', axis=1)
+
+        with open("data/hko_{}.csv".format(year), 'w') as csvfile:
+            df.to_csv(csvfile, index=False, line_terminator='\n')
